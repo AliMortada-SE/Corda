f6695012e64f733ad41b2b5d94e90c2940af22e5
AliMortada-SE <alimortada.se@gmail.com>
2026-04-11 20:33:13 +0300

    remove func. added

--- corda.h
+++ corda.h
@@ -7,10 +7,10 @@
 #include <vector>
 class CORDA{
 public:
-std::pair<int,int> find(std::string target, std::string line){
-    size_t pos = line.find(target);
+std::pair<int,int> find(std::string key, std::string line){
+    size_t pos = line.find(key);
     while(pos != std::string::npos){
-        size_t after = pos + target.size();
+        size_t after = pos + key.size();
         // skip spaces
         while(after < line.size() && line[after] == ' ') after++;
         // bounds check
@@ -21,17 +21,25 @@ std::pair<int,int> find(std::string target, std::string line){
             return {(int)pos, (int)(after + 1)};
         }
         // search again after this occurrence
-        pos = line.find(target, pos + 1);
+        pos = line.find(key, pos + 1);
     }
     return {-1, -1};
 }
-bool isExist(std::string target,std::string line){
-    std::pair<int,int> offset = find(target,line);
+int sizeOf(int index,std::string line){
+    int size = 0;
+    while(index<line.size() && line[index] != ';'){
+        size++;
+        index++;
+    }
+    return size;
+}
+bool isExist(std::string key,std::string line){
+    std::pair<int,int> offset = find(key,line);
     if(offset.first == -1 && offset.second == -1) return 0;
     return 1;
 }
-std::string get(std::string target,std::string line){
-    std::pair<int,int> offset = find(target,line);
+std::string get(std::string key,std::string line){
+    std::pair<int,int> offset = find(key,line);
     if(offset.first == -1 && offset.second == -1) return "";
     int x = offset.second;
     std::string value;
@@ -41,56 +49,66 @@ std::string get(std::string target,std::string line){
     }
     return value;
 }
-std::string add(std::string tag,std::string value){
-    return tag + ':' + value + ';';
+std::string add(std::string key,std::string value){
+    return key + ':' + value + ';';
 }
-int locateIndex(std::string target,std::string type, std::string line){
-    std::string Target = get(type,line);
-    Target+=';';
+std::string remove(std::string key, std::string& line){
+    std::pair<int,int> offset = find(key,line);
+    if(offset.first == -1 && offset.second == -1) return "";
+    int start = offset.first;
+    int end   = sizeOf(offset.second,line);
+    if("")
+    line.erase(start,(offset.second - offset.first) + end + 1);
+    return line;
+}
+
+int locateIndex(std::string key,std::string type, std::string line){
+    std::string Key = get(type,line);
+    Key+=';';
     std::string temp;
     int counter = 0;
     int x = 0;
-    while(x<Target.size()){
-        while(x < Target.size() && Target[x]!=','){
-            temp+=Target[x];
+    while(x<Key.size()){
+        while(x < Key.size() && Key[x]!=','){
+            temp+=Key[x];
             x++;
         }
         x++;
-        if(temp == target){
+        if(temp == key){
             return counter;
         }
         else{
-            if(x >= Target.size() || Target[x]==';') return -1;
+            if(x >= Key.size() || Key[x]==';') return -1;
             temp.clear();
             counter++;
         }
     }
     return -1;
 }
-std::string getByIndex(std::string target, std::string line, int index){
-    std::string Target = get(target,line);
-    Target+=';';
+std::string getByIndex(std::string key, std::string line, int index){
+    std::string Key = get(key,line);
+    Key+=';';
     int counter = 0;
     int x=0;
     std::string temp;
-    while(counter != index && x<Target.size()){
-        while(Target[x]!=','&& x<Target.size()){
+    while(counter != index && x<Key.size()){
+        while(Key[x]!=','&& x<Key.size()){
             x++;
         }
         x++;
         counter++;
     }
     if(counter==index){
-        while(Target[x]!=',' && Target[x]!=';' && x<Target.size()){
-            temp+=Target[x];
+        while(Key[x]!=',' && Key[x]!=';' && x<Key.size()){
+            temp+=Key[x];
             x++;
         }
     }
     return temp;
 }
-std::string createArray(std::string tag, std::vector<std::string> list){
+std::string createArray(std::string key, std::vector<std::string> list){
     std::string array;
-    array += tag + ':';
+    array += key + ':';
     int x = 0;
     while(x<list.size()){
         array += list[x];
--- main.cpp
+++ main.cpp
@@ -2,11 +2,7 @@
 #include <iostream>
 CORDA corda;
 int main(){
-    std::string line = "name:Ali,John,Mark,;age:21,27,18,;live:IQ,US,UK,;";
-    int index = corda.locateIndex("John","name",line);
-    std::string age = corda.getByIndex("live",line,index);
-    std::string getAges = corda.get("age",line);
-    std::cout<<"All Ages:"<<getAges<<"\n";
-    std::cout<<"Age:"<<age<<"\n";
+    std::string line = "name:Ali;age:21;";
+    std::cout << corda.remove("name",line)<<"\n";
     return 0;
 }
